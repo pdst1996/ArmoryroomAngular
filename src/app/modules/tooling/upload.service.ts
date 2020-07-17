a33c3f143ee60f106bf66759e1ca7c1cafa3ff25
pedrodaniel_saldana <pedrodaniel.saldana@sanmina.com>
2020-07-17 11:04:55 -0500

    File 1

--- src/app/modules/tooling/upload.service.ts
+++ src/app/modules/tooling/upload.service.ts
@@ -1,6 +1,7 @@
 
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Constants } from "src/app/helpers/constats";
 
 
 @Injectable({
@@ -9,10 +10,13 @@ import { Injectable } from "@angular/core";
 
 export class UploadService {
 
+    private url = `${Constants.SERVER}upload/file/daniel.jpg`;
+
     constructor(private httpClient: HttpClient) { }
 
     public upload(formData) {
-        return this.httpClient.post<any>("", formData, {  
+        console.log(formData)
+        return this.httpClient.post<any>(this.url, formData, {  
           reportProgress: true,  
           observe: 'events'  
         });  
